9fb1f939970618d844dac34bf97276450271e0fc
Ashwin Srinath <shwina@users.noreply.github.com>
2020-08-24 07:42:14 -0400

    Use par directly

--- stdpar_sort.pyx
+++ stdpar_sort.pyx
@@ -2,13 +2,13 @@
 # distutils: extra_compile_args=-fPIC -stdpar=gpu -gpu=nordc
 # distutils: extra_link_args=-shared -stdpar=gpu
 
-from libcpp.execution cimport parallel_policy
+from libcpp.execution cimport par
 from libcpp.algorithm cimport sort
 from libcpp.vector cimport vector
 from libcpp.utility cimport move
 
 cdef vector[double] x
 x = [9, 8, 7]
-sort(parallel_policy(), x.begin(), x.end())
+sort(par, x.begin(), x.end())
 print(x)
 
