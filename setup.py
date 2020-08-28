668fd11a7df0a08201926a82a15e8c0beafabc27
Ashwin Srinath <shwina@users.noreply.github.com>
2020-08-28 16:44:43 -0400

    Handle both g++ and nvc++ compilation in the same setup.py for convenience

--- setup.py
+++ setup.py
@@ -1,54 +1,71 @@
 import  os
 import shutil
-from os.path import join as pjoin
 from setuptools import setup, Extension
-import subprocess
-import sys
 from setuptools.command.build_ext import build_ext
-from collections import defaultdict
+from Cython.Build import cythonize
 
 import numpy as np
 
 
+include_dirs = [np.get_include()]
+library_dirs = []
+
+
+# Try to find nvc++:
 NVCPP_EXE = shutil.which("nvc++")
-if not NVCPP_EXE:
+if NVCPP_EXE is None:
     NVCPP_EXE = os.environ.get("CC", None)
-NVCPP_HOME = os.path.dirname(os.path.dirname(NVCPP_EXE))
-NVCPP_INC_DIRS = [
-    os.path.join(NVCPP_HOME, "include-stdpar")
-]
-NVCPP_LIB_DIRS = [
-    os.path.join(NVCPP_HOME, "lib")
-]
+
+if NVCPP_EXE is not None:
+    NVCPP_EXE = NVCPP_EXE if NVCPP_EXE.endswith("nvc++") else None
+
+if NVCPP_EXE is not None:
+    NVCPP_HOME = os.path.dirname(os.path.dirname(NVCPP_EXE))
+    include_dirs += [
+        os.path.join(NVCPP_HOME, "include-stdpar")
+    ]
+    library_dirs += [
+        os.path.join(NVCPP_HOME, "lib")
+    ]
+
 
 class custom_build_ext(build_ext):
     def build_extensions(self):
-        # Override the compiler executables. Importantly, this
-        # removes the "default" compiler flags that would
-        # otherwise get passed on to nvc++, i.e.,
-        # distutils.sysconfig.get_var("CFLAGS"). nvc++
-        # does not support all of those "default" flags
-        self.compiler.set_executable("compiler_so", NVCPP_EXE)
-        self.compiler.set_executable("compiler_cxx", NVCPP_EXE)
-        self.compiler.set_executable("linker_so", NVCPP_EXE)
+        if NVCPP_EXE:
+            # Override the compiler executables. Importantly, this
+            # removes the "default" compiler flags that would
+            # otherwise get passed on to nvc++, i.e.,
+            # distutils.sysconfig.get_var("CFLAGS"). nvc++
+            # does not support all of those "default" flags
+            compile_args = "-fPIC -stdpar -gpu=nordc -std=c++17"
+            link_args = "-shared -stdpar"
+            self.compiler.set_executable(
+                "compiler_so",
+                NVCPP_EXE + " " + compile_args
+            )
+            self.compiler.set_executable("compiler_cxx", NVCPP_EXE)
+            self.compiler.set_executable(
+                "linker_so",
+                NVCPP_EXE + " " + link_args
+            )
         build_ext.build_extensions(self)
-        
-from Cython.Build import cythonize
+
+
 ext = cythonize([
     Extension(
         '*',
         sources=['*.pyx'],
-        include_dirs=NVCPP_INC_DIRS + [np.get_include()],
-        library_dirs=NVCPP_LIB_DIRS,
-        runtime_library_dirs=NVCPP_LIB_DIRS,
-        extra_compile_args=["-fPIC", "-stdpar", "-gpu=nordc", "-std=c++17"],
-        extra_link_args=["-shared", "-stdpar"]
+        include_dirs=include_dirs,
+        library_dirs=library_dirs,
+        runtime_library_dirs=library_dirs,
+        extra_compile_args=["-std=c++17"]
     )])
 
-setup(name='cppsort',
-      author='Ashwin Srinath',
-      version='0.1',
-      ext_modules = ext,
-      zip_safe=False,
-      cmdclass={'build_ext': custom_build_ext }
+setup(
+    name='cppsort',
+    author='Ashwin Srinath',
+    version='0.1',
+    ext_modules=ext,
+    zip_safe=False,
+    cmdclass={'build_ext': custom_build_ext}
 )
