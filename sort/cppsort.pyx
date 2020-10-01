cc0b8b7971e68c0db82970ea07d1085fee7c305b
Ashwin Srinath <shwina@users.noreply.github.com>
2020-10-01 14:39:34 -0400

    Sort a vector of ints for simplicity

--- sort/cppsort.pyx
+++ sort/cppsort.pyx
@@ -4,10 +4,8 @@ from libcpp.algorithm cimport sort, copy
 from libcpp.vector cimport vector
 
 
-def cppsort(double[:] x):
-    cdef vector[double] vec
-    vec.reserve(len(x))
-    vec.resize(len(x))
+def cppsort(int[:] x):
+    cdef vector[int] vec = vector[int](len(x))
     copy(&x[0], &x[-1], vec.begin())
     sort(vec.begin(), vec.end())
     copy(vec.begin(), vec.end(), &x[0])
